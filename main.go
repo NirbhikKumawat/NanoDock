2b07e2faebc9d924e51af118cea5dd7f9c03f3c4
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-12-21 09:48:09 +0530

    added cursor navigation uo and down in main view as well

--- main.go
+++ main.go
@@ -332,6 +332,12 @@ func keybindings(g *gocui.Gui) error {
 	if err := g.SetKeybinding("help", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
 		return err
 	}
+	if err := g.SetKeybinding("body", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("body", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
+		return err
+	}
 	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		return err
 	}
